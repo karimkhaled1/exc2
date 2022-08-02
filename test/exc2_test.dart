f9737045fce97905478bbb43aa4c23cf04849822
karim khaled ahmed <karimkhaled60@gmail.com>
2022-08-02 20:01:13 +0200

    renaming test

--- test/exc2_test.dart
+++ test/exc2_test.dart
@@ -12,34 +12,27 @@ void main() {
       Product("id4", "area4", "name2", 3, "brand3")
     ];
   });
-  group("description", () {
-    test('avarage', () {
-      expect(avarage(products)['name1'], equals(2));
-      expect(avarage(products)['name2'], equals(0.75));
-    });
+
+  test('avarage', () {
+    expect(avarage(products)['name1'], equals(2));
+    expect(avarage(products)['name2'], equals(0.75));
   });
 
-  group("pop brand", () {
-    test('avarage', () {
-      expect(getPopularBrand(products)['name1'], equals("brand1"));
-      expect(getPopularBrand(products)['name2'], equals("brand3"));
-    });
+  test('PopularBrand', () {
+    expect(getPopularBrand(products)['name1'], equals("brand1"));
+    expect(getPopularBrand(products)['name2'], equals("brand3"));
   });
-  group("fileName", () {
-    test('fileName', () {
-      expect(getFileName("d/test/bla"), equals("bla"));
-      expect(getFileName(r"d\test\bla"), equals("bla"));
-    });
+  test("fileName parsing", () {
+    expect(getFileName("d/test/bla"), equals("bla"));
+    expect(getFileName(r"d\test\bla"), equals("bla"));
   });
 
-  group("fileName", () {
-    test('fileName', () {
-      var testedProduct = Product.fromCSV("ID1,Minneapolis,shoes,2,Air");
-      expect(testedProduct.id, equals("ID1"));
-      expect(testedProduct.area, equals("Minneapolis"));
-      expect(testedProduct.name, equals("shoes"));
-      expect(testedProduct.quantity, equals(2));
-      expect(testedProduct.brand, equals("Air"));
-    });
+  test('from csv parsing', () {
+    var testedProduct = Product.fromCSV("ID1,Minneapolis,shoes,2,Air");
+    expect(testedProduct.id, equals("ID1"));
+    expect(testedProduct.area, equals("Minneapolis"));
+    expect(testedProduct.name, equals("shoes"));
+    expect(testedProduct.quantity, equals(2));
+    expect(testedProduct.brand, equals("Air"));
   });
 }
